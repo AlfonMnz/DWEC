4df6b726180fb8c4899b92fb3c84b1c56dd18dff
PitoEuropeo <alfonmusan14@gmail.com>
2017-09-26 12:19:22 +0200

    first commit

---
+++ prueba.js
@@ -0,0 +1,8 @@
+alert("Tengo un moco, me lo saco poco a poco, lo redondeo, lo miro con deseo," +
+    " me lo como y como sabe a poco cojo el dedo y saco otro moco");
+document.write("Me lo cargo");
+console.log("Antonio Terrero Algaba es el mejor del mundo");
+
+var a;
+
+console.log("The value of a is " +  a);
