66ed0a139cc15bccaf9cc542cfd5ae701fa829b2
PitoEuropeo <alfonmusan14@gmail.com>
2017-09-26 14:28:05 +0200

    relacion5 ejercicio 15 (a completar)

--- 1ºTrimestre/Relacion5/ejercicio4.14.js
+++ 1ºTrimestre/Relacion5/ejercicio4.14.js
@@ -3,12 +3,13 @@ var ancho = window.prompt("Introduce el número de píxeles de ancho");
 var alto = window.prompt("Introduce el número de píxeles de alto");
 
 function createTablas(numColumnas, ancho, alto) {
-    var i = 0;
+    var i = 0
     document.write("<table border = '0' cellspacing = '2' bgcolor = 'black' width = '" + ancho * numColumnas + "'> ")
     document.write("<tr bgcolor='white' height = '" + alto + "' ")
     while (i < numColumnas) {
-        document.write("<td width = '" + ancho + "'> &nbsp;</td>")
-        i++
+        document.write("<td width = '" + ancho + "' bgcolor='black'> &nbsp;</td>")
+        document.write("<td width = '" + ancho + "' bgcolor='white'> &nbsp;</td>")
+        i += 2;
     }
     document.write("</tr>")
     document.write("</table>")
---
+++ 1ºTrimestre/Relacion5/ejercicio4.15.js
@@ -0,0 +1,19 @@
+var numero = window.prompt("Jugador 1: Introduza un número.");
+
+function mayor_o_menor(numero, num_a_comprobar) {
+    if (num_a_comprobar > numero) {
+        alert("El jugador 1 ha elegido un número menor")
+    }
+    else {
+        alert("El jugador 1 ha elegido un número mayor")
+    }
+    var num_a_comprobar = window.prompt("INTRODUSE UN NUMEROSIKO AEH?");
+}
+
+
+while (numero != num_a_comprobar){
+
+    mayor_o_menor(numero, num_a_comprobar);
+
+}
+alert("ENHORABUENA SOSIO")
