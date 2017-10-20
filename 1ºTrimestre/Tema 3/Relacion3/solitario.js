75d24d990a87d107e578c778029de4ad7af7d8dc
PitoEuropeo <alfonmusan14@gmail.com>
2017-10-20 13:03:38 +0200

    Relación 3 Solitario Spider: Barajados y mostrados, organizar en columnas.

--- 1ºTrimestre/Tema 3/Relacion3/solitario.js
+++ 1ºTrimestre/Tema 3/Relacion3/solitario.js
@@ -11,23 +11,41 @@ class Baraja {
             this.array_montones.push(new Monton(i))
         }
     }
-//  printear(){
-//
-//
-//        var output = '';
-//        for (var property in object) {
-//            output += property + ': ' + object[property]+'; ';
-//        }
-//
-//
-//
-//    }
-    organizar_baraja() {
-        document.write("<ul>Primer monton:")
-        for  (let i=0;i<=this.array_montones[1].array_cartas.length;i++){
-            document.write("<li>holi </li>")
+
+
+    static shuffle(a) {
+        let j
+        for (let i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+
         }
-        document.write("</ul>")
+        return a
+    }
+
+    static mostrar_carta(carta) {
+        var status = Math.floor(Math.random() * 2)
+        if (status === 1) {
+            carta.status = true;
+        }
+    }
+
+    organizar_baraja() {
+        for (let i = 0; i < this.array_montones.length; i++) {
+            document.write("<ul>montón " + i);
+            Baraja.shuffle(this.array_montones[i].array_cartas)
+            for (let j = 0; j < this.array_montones[i].array_cartas.length; j++) {
+                Baraja.mostrar_carta(this.array_montones[i].array_cartas[j])
+                if (this.array_montones[i].array_cartas[j].status === false) {
+                    document.write("<li> ||  </li>")
+                }
+                else
+
+                    document.write("<li>" + this.array_montones[i].array_cartas[j].numero + " || " + this.array_montones[1].array_cartas[j].palo + "</li>")
+            }
+            document.write("</ul>")
+        }
+
     }
 }
 
@@ -38,8 +56,10 @@ class Monton {
     }
 
     crear_monton(palo) {
-        for (let i = 1; i <= 14; i++) {
-            this.array_cartas.push(new Carta(i, i))
+
+        for (let i = 1; i <= 13; i++) {
+            this.array_cartas.push(new Carta(i, 1))
+
         }
     }
 
@@ -47,9 +67,16 @@ class Monton {
 
 class Carta {
     constructor(numero, palo = 0) {
-        this.numero = numero;
-        this.palo = palo;
+        if (numero === 11) this.numero = 'J';
+        else if (numero === 12) this.numero = 'Q';
+        else if (numero === 13) this.numero = 'K';
+        else this.numero = numero;
+        if (palo === 1) this.palo = '♠';
+        else if (palo === 2) this.palo = '♦';
+        else if (palo === 3) this.palo = '♥';
+        else this.palo = '♣';
+        this.status = false;
     }
 }
 
-b = new Baraja(4);
+b = new Baraja(90)
