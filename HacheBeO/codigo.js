1997806a766462ea51e1e93102980969d0695126
PitoEuropeo <alfonmusan14@gmail.com>
2018-01-15 10:23:45 +0100

    Omdb api

--- HacheBeO/codigo.js
+++ HacheBeO/codigo.js
@@ -4,15 +4,22 @@
 
 var apikey = 'b8187916'
 let c;
+let page = 1
 
 class Pelicula {
     constructor(poster, title, type, year, ImbdID) {
         this.poster = poster;
         this.title = title;
+        this.type = type;
         this.ImbdID = ImbdID;
         this.year = year
+        this.foto()
+    }
 
-
+    foto() {
+        if (this.poster == "N/A") {
+            this.poster = "http://atalgaba.com/img/xatalgaba.jpg.pagespeed.ic.Gx-px_oYWl.jpg"
+        }
     }
 }
 
@@ -26,11 +33,19 @@ class coleccionPeliculas {
     }
 }
 
-function obtenerPeliculas() {
+function vagino() {
+
+    limpiarPeliculas()
+    obtenerPeliculas(1)
+
+}
+
+function obtenerPeliculas(paje) {
+
     var texto = $('input').val()
 
     $.ajax({
-        url: "http://www.omdbapi.com/?s=" + texto + "&apikey=" + apikey,
+        url: "http://www.omdbapi.com/?s=" + texto + "&apikey=" + apikey + "&page=" + paje,
         success: function (res) {
             var array_res = res;
 
@@ -41,12 +56,8 @@ function obtenerPeliculas() {
 
 
 function crearPeliculas(peliculas) {
-    console.log(peliculas)
-    console.log("crearPeliculas")
-    console.log(peliculas.Search.length)
     for (let i = 0; i < peliculas.Search.length; i++) {
-        console.log(peliculas.Search[i])
-        c.meterPelicula(new Pelicula(peliculas.Search[i].Poster, peliculas.Search[i].Title, peliculas.Search[i].Type, peliculas.Search[i].Year, peliculas.Search[i].imbdID))
+        c.meterPelicula(new Pelicula(peliculas.Search[i].Poster, peliculas.Search[i].Title, peliculas.Search[i].Type, peliculas.Search[i].Year, peliculas.Search[i].imdbID))
     }
     enmaquetarPeliculas()
 
@@ -56,14 +67,32 @@ function crearPeliculas(peliculas) {
 function enmaquetarPeliculas() {
     let peliculas = c.arrayPeliculas;
     for (let i = 0; i < peliculas.length; i++) {
-        $('#cuerpo').append('<div class="pelicula"><img id="' + peliculas[i].ImdbID + '" src="' + peliculas[i].poster + '"><h2>' + peliculas[i].title + '</h2><p>' + peliculas[i].year + '</p></div>');
+        $('#cuerpo').append('<div class="pelicula"><a href="http://www.imdb.com/title/'+peliculas[i].ImbdID+'"><img id="' + peliculas[i].ImdbID + '" src="' + peliculas[i].poster + '"></a><p>' + peliculas[i].title + '</p><p>' + peliculas[i].year + '</p></div>');
 
     }
 }
 
 function limpiarPeliculas() {
+
+    $('#cuerpo').empty()
     c = new coleccionPeliculas()
     console.log(c.arrayPeliculas)
 }
 
 c = new coleccionPeliculas();
+$(document).ready(function () {
+    var win = $(window);
+
+    // Each time the user scrolls
+    win.scroll(function () {
+        // End of the document reached?
+        if ($(document).height() - win.height() == win.scrollTop()) {
+            page++
+            console.log(page)
+            obtenerPeliculas(page)
+
+
+            ;
+        }
+    });
+});
